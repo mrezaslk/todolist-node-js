15945a2135e7734e0d735ee04e76a3d9d17af377
mohammadreza salek <rezasalek75@gmail.com>
2023-04-03 15:30:40 +0330

    [wip]add bootstrap

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 // MVC Model View Controller
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const { setStatics } = require('./utils/statics');
 
 
 const app = express();
-
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+// end Middleware
+
+// EJS
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+// End EJS
+
+// Static 
+
+setStatics(app);
+// Routes
+app.get("/", (req, res) => {
+    res.render('index', { pageTitle: "کار های روزمره" })
+})
+// end Routes
 app.listen(3000, () => console.log(`Server is running`))
